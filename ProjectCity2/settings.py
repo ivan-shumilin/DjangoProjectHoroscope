6a0ad500f7eb0848ac6649521aa73d707c291455
ivan-shumilin <shumilin.i.v@gmail.com>
2022-01-09 11:05:45 +0300

    add models calendar_id and element_id

--- ProjectCity2/settings.py
+++ ProjectCity2/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'horoscope',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
--- horoscope/models.py
+++ horoscope/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Zodiak_sing(models.Model):
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=45)
+    description = models.CharField(max_length=255)
+    element = models.ForeignKey('element', on_delete=models.PROTECT, null=True)
+    calendar = models.OneToOneField('calendar', on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return f'{self.name} - {self.description}'
+
+
+class element(models.Model):
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=45)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.name} - {self.description}'
+
+
+class calendar(models.Model):
+    month_from = models.SmallIntegerField(null=True)
+    month_to = models.SmallIntegerField(null=True)
+    day_from = models.SmallIntegerField(null=True)
+    day_to = models.SmallIntegerField(null=True)
+
+    def __str__(self):
+        return f'{self.month_from} - {self.month_to} - {self.day_from} - {self.day_to}'
