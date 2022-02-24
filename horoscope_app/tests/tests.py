40484cab7b3659c1ab5ee00060a674b1fda4fa62
Andrey Larin <a.larin@corp.mail.ru>
2022-02-24 23:48:41 +0300

    Add unit test and code review

--- horoscope_app/tests/tests.py
+++ horoscope_app/tests/tests.py
@@ -1,20 +1,44 @@
-# # from horoscope_app import views
-# import pytest
-# import datetime
-# from DjangoProjectHoroscope.horoscope_app.models import ZodiakSing
-# from django import forms
-# мне надо передать в форму 2 значения с датами и сравнить ответ с expected
+from horoscope_app import views
+import datetime
 
-# @pytest.mark.parametrize("date,expected",
-#     [(datetime.date(2012, 10, 14), "libra"),
-#      (datetime.date(2012, 11, 14), "scorpio")])
-# def test_search_sing_by_date(date, expected):
-#     assert views.search_sing_by_date(date) == expected
+from horoscope_app.forms import ZodiakSingForm
+from horoscope_app.models import ZodiakSing
 
-# def test_search_sing_by_date():
-#     date = datetime.date(2000, 3, 22)
-#     assert views.search_sing_by_date(date) == "aries"
+from django.test import TestCase
 
-def test_horoscope():
-    pass
 
+class TestHoroscope(TestCase):
+    fixtures = ['horoscope_app/tests/fixtures/horoscope.json']
+
+    def assert_search_sing_by_date(self, date, sign):
+        self.assertEqual(views.search_sing_by_date(date), sign)
+
+    def test_search_sing_by_date(self):
+        self.assert_search_sing_by_date(datetime.date(2012, 1, 1), 'capricorn')
+        self.assert_search_sing_by_date(datetime.date(2012, 10, 14), 'libra')
+        self.assert_search_sing_by_date(datetime.date(2012, 11, 14), 'scorpio')
+        self.assert_search_sing_by_date(datetime.date(2012, 3, 1), 'pisces')
+
+    # def test_calendar_form_errors(self):
+    #     form = ZodiakSingForm(data={'date_from': '2022-15-02'})
+    #     self.assertEqual('Enter a valid date.', form.errors['date_from'][0])
+
+    def test_calendar_errors(self):
+        response = self.client.post('/calendar/', data={'date_from': '2022-15-02'})
+        self.assertEqual(200, response.status_code)
+        self.assertEqual('Enter a valid date.', response.context['form'].errors['date_from'][0])
+        self.assertEqual('Некорректные данные', response.context['error'])
+        # Поиск в html
+        self.assertIn('Поиск знака зодиака по дате', response.content.decode('utf-8'))
+        self.assertIn('Некорректные данные', response.content.decode('utf-8'))
+
+    def test_calendar(self):
+        response = self.client.post('/calendar/', data={'date_from': '2022-01-02'})
+        self.assertEqual(302, response.status_code)
+        self.assertRedirects(response, '/capricorn')
+
+    def test_get_zodiac_sign(self):
+        response = self.client.get('/capricorn', data={'date_from': '2022-01-02'})
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(response.context['zodiac_sing'].id, ZodiakSing.objects.get(code='capricorn').id)
+        self.assertEqual(len(response.context['zodiac_sings']), 12)
