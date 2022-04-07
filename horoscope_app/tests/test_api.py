a211567c4c1a000d0ecf433b918a4e1f483092db
ivan-shumiln <shumilin.i.v@gmail.com>
2022-04-07 23:23:28 +0300

    Добавил тест на соответствие всех знаков в ответе

--- horoscope_app/tests/test_api.py
+++ horoscope_app/tests/test_api.py
@@ -1,10 +1,17 @@
 from horoscope_app import views
+from rest_framework.test import APITestCase
 
 from django.test import TestCase
+import pdb
 
 
 class ForecastsApiTestCase(APITestCase):
     def test_get_forecast_for_api(self):
         response = views.get_forecast_for_api()
-        self.assertEqual(len(response), 13)
+        #pdb.set_trace()
+        self.assertEqual(len(response), 13) # количество знаков 13 (12 + general)
+        all_sing = set(['general', 'Овен', 'Телец', 'Близнецы', 'Рак', 'Лев', 'Дева', 'Весы', 'Скорпион', 'Стрелец', 'Козерог', 'Водолей', 'Рыбы'])
+        response_all_sing = set([dict['sing'] for dict in response])
+        self.assertEqual(all_sing, response_all_sing) # Проверяем наличие всех знаков в ответе
+
 
